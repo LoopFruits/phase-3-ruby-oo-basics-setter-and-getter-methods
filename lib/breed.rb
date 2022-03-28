614510f8ae4cd28fb19d2e24753dbb1ee1152048
LoopFruits <Kensworldc@gmail.com>
2022-03-27 22:37:43 -0400

    commit tree 921756d00b236ee77d7ea77007fe27890ff7a01f

---
+++ lib/breed.rb
@@ -0,0 +1,9 @@
+class Breed
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed 
+        @breed
+    end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require_relative '../lib/dog'
 require_relative '../lib/person'
+require_relative '../lib/breed'
 
 RSpec.configure do |config|
   config.order = :default
